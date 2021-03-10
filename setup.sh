b0b407051f7b9ac672ab2403ce2177f167482091
Chaewon Kang <ckang@c3r7s7.42seoul.kr>
2021-03-10 09:36:00 +0900

    rm readme

--- setup.sh
+++ setup.sh
@@ -3,13 +3,17 @@ minikube start --driver=virtualbox
 
 #To point your shell to minikube's docker-daemon, run:
 eval $(minikube docker-env)
+
 #
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+
 # On first install only
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
 #
 kubectl apply -f ./srcs/yaml_config/metallb.yaml
+
 #
 docker build -t nginx       ./srcs/image_config/nginx
 docker build -t phpmyadmin  ./srcs/image_config/phpmyadmin
@@ -18,6 +22,7 @@ docker build -t mysql       ./srcs/image_config/mysql
 docker build -t influxdb    ./srcs/image_config/influxdb
 docker build -t grafana     ./srcs/image_config/grafana
 docker build -t ftps        ./srcs/image_config/ftps
+
 # 
 kubectl apply -f ./srcs/yaml_config/nginx.yaml
 kubectl apply -f ./srcs/yaml_config/mysql.yaml
